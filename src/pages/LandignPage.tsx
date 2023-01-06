03ca9ffe4907175c1b816622a4ffc3ddb10ea666
David <david.cicconi94@gmail.com>
2023-01-06 11:19:31 -0300

    creating firebase config

--- src/pages/LandignPage.tsx
+++ src/pages/LandignPage.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const LandignPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Landing Page</h2>
+    </div>
+  );
 };
 
 export default LandignPage;
